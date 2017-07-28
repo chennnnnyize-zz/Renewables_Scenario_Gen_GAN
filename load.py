3cb4385a8945219685c5551203e59a3e9cbbf905
Yize Chen <yizechen@uw.edu>
2017-07-28 10:53:57 -0700

    Update load.py

--- load.py
+++ load.py
@@ -5,7 +5,7 @@ sys.path.append('..')
 import numpy as np
 import os
 
-#Load .csv renewables data into GANs model
+#Load .csv or .txt renewables data into GANs model
 #Currently use power generation historical data from NREL, which can be downloaded from NREL wind or solar integration datasets
 #Historical data are loaded by column sequence and reshape into model input shape, which is adjustable
 #Label is only useful for event-based scenario generation
@@ -33,7 +33,7 @@ def load_wind():
             trX = np.concatenate((trX, train), axis=0)
     print("Shape TrX", shape(trX))
 
-    with open('sample label.csv', 'r') as csvfile:
+    with open('wind label.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         rows = [row for row in reader]
     label = np.array(rows, dtype=int)
@@ -41,6 +41,36 @@ def load_wind():
     print("Label shape", shape(label))
     return trX, label
 
+def load_wind_data_spatial():
+    with open('spatial.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = [row for row in reader]
+    rows = np.array(rows, dtype=float)
+    m = np.ndarray.max(rows)
+    print("Maximum value of wind", m)
+    rows=rows/m
+    return rows
 
+
+def load_solar_data():
+    with open('solar_label.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = [row for row in reader]
+    labels = np.array(rows, dtype=int)
+    print(shape(labels))
+
+    with open('solar.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = [row for row in reader]
+    rows = np.array(rows, dtype=float)
+    rows=rows[:104832,:]
+    print(shape(rows))
+    trX = np.reshape(rows.T,(-1,576))
+    print(shape(trX))
+    m = np.ndarray.max(rows)
+    print("maximum value of solar power", m)
+    trY=np.tile(labels,(32,1))
+    trX=trX/m
+    return trX,trY
 
 
